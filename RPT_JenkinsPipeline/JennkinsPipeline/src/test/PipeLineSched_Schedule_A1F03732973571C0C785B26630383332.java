4a29abd4590f0f382d7f1bd6684a4300a857e0b9
tomasiel <ltomasiello2024@gmail.com>
2025-05-22 15:11:50 -0400

    Ignore .eclipse/ directory

---
+++ RPT_JenkinsPipeline/JennkinsPipeline/src/test/PipeLineSched_Schedule_A1F03732973571C0C785B26630383332.java
@@ -0,0 +1,98 @@
+/**@generated
+ * WARNING � Changes you make to this file may be lost.
+ *           File is generated and may be re-generated without warning.
+ * @RPT-Core-generated Version 8.6
+ */
+package test;
+
+import com.ibm.rational.test.lt.kernel.action.IContainer;
+import com.ibm.rational.test.lt.kernel.action.IKThinkControl;
+import com.ibm.rational.test.lt.kernel.action.impl.Container;
+import com.ibm.rational.test.lt.kernel.action.impl.KThrow;
+import com.ibm.rational.test.lt.kernel.action.impl.UserGroup;
+import com.ibm.rational.test.lt.kernel.data.FeatureOptionsDataAreaCreation;
+import com.ibm.rational.test.lt.kernel.services.*;
+import com.ibm.rational.test.lt.kernel.services.RPTEventStructure;
+import java.util.HashMap;
+
+@SuppressWarnings("all")
+public class PipeLineSched_Schedule_A1F03732973571C0C785B26630383332
+		extends com.ibm.rational.test.lt.kernel.action.impl.Schedule {
+
+	public PipeLineSched_Schedule_A1F03732973571C0C785B26630383332(IContainer parent, String name) {
+		super(parent, name, "A1F03732973571C0C785B26630383332");
+		super.actualScheduleName = "PipeLineSched";
+		setThinkMax(10000);
+		setThinkScheme(IKThinkControl.RECORDED);
+
+	}
+
+	public void execute() {
+
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTFailVPEvent(), null, 1, "Content Verification Point failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTConnectEvent(), null, 1, "Connection failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTAuthenticationEvent(), null, 1, "Authentication failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTDataPoolEOFEvent(),
+				new RPTStopUserEvent("End of dataset reached"), 1, "End of dataset reached"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTTestVerdictFailedEvent(), null, 2, "Test verdict was set to fail"));
+		this.addEventBehavior(new RPTEventStructure(new RPTReferenceEvent(), null, 1, "Failed to extract reference"));
+		this.addEventBehavior(new RPTEventStructure(new RPTSubstitutionEvent(), null, 1, "Substitution failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTServerTimeoutEvent(), null, 1, "Timeout"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTDroppedConnectionEvent(), null, 1, "Server dropped connection"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTCustomCodeVPFailureEvent(), null, 1, "Custom Verification Point failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeExceptionEvent(),
+				new RPTStopUserEvent("Custom Code reported an unhandled exception"), 1,
+				"Custom Code reported an unhandled exception"));
+
+		this.addUserGroup(new UserGroup_1(this));
+
+		setMaxHealthFailuresToLog(50, -1, 0);
+		super.execute();
+	}
+
+	public class UserGroup_1 extends UserGroup {
+
+		public UserGroup_1(IContainer parent) {
+			super(parent, "User Group 1", "A1F03732973E2454C785B26630383332");
+
+			setNumUsers(100.0);
+
+		}
+
+		public IContainer createTesterWorkload(IContainer parent) {
+			return scenario_1(parent);
+		}
+
+		private Container scenario_1(IContainer parent) {
+			Container scenario = new Container(parent, "Default Scenario", "A1F03732973E7274C785B26630383332") {
+
+				public void reportStopMessage() {
+				}
+
+				public void reportForcedStopMessage() {
+				}
+
+				public void execute() {
+					this.add(new test.PipelineTest_Test_A1F037328D3BBBC0C785B26630383332(this,
+							"A1F037329BA8D170C785B26630383332") {
+						public void execute() {
+							this.setRtbEnabled(false);
+							super.execute();
+						}
+					});
+
+					this.add(new com.ibm.rational.test.lt.execution.protocol.impl.HTTPUserComplete(this));
+					super.execute();
+				}
+			};
+
+			return scenario;
+		}
+
+	}
+
+}
